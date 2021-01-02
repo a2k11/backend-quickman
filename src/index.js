6fe7aabd27ad70123aba90995769e9f363ff41e0
a2k11 <akaewell@xometry.com>
2021-01-02 13:55:12 -0500

    setting up prisma2 client & cli, created db for backend, added simple migration & prisma client for data entry, created envs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Adam',
+      email: 'adam.kaewell@gmail.com',
+    },
+  })
+  const allUsers = await prisma.user.findMany({})
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
