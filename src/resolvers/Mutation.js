97e2cab0f6315ef75ac7f01c76ac6b63d8980dce
a2k11 <akaewell@xometry.com>
2021-01-07 23:56:29 -0500

    adding prisma model date times, fixed prisma client mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,6 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-
-    const item = await ctx.prisma.post.createItem({
+    const item = await ctx.prisma.item.create({
       data: {
         ...args
       }
