d0e2b16570c09e123ca9e95f4516a2615a22c89b
a2k11 <akaewell@xometry.com>
2021-01-08 23:56:18 -0500

    adding query resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,7 @@
 const Query = {
-  items(parent, args, ctx, info) {
-    return [];
+  async items(parent, args, ctx, info) {
+    const items = await ctx.prisma.item.findMany();
+    return items;
   },
 };
 
